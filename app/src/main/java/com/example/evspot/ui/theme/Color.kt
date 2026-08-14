b8535620744d59440403b8aa125ca09e4b44e0d2
MJ1459 <mihirjagtapn257@gmail.com>
2026-08-14 12:20:16 +0530

    Build VoltWay vehicle details screen

--- app/src/main/java/com/example/evspot/ui/theme/Color.kt
+++ app/src/main/java/com/example/evspot/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.example.evspot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val VoltGreen = Color(0xFF0A8F4D)
+val PaleGreen = Color(0xFFEDF9F1)
+val BackgroundGray = Color(0xFFF8F8F8)
+val DarkNavy = Color(0xFF0D1B2A)
+val SecondaryGray = Color(0xFF6C757D)
+val ErrorRed = Color(0xFFDC3545)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
